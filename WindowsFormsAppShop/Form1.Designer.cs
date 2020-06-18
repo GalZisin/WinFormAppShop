436f2cecb28382e8f1a7bf4b5ef35a3778efb56c
Gal Zisin <galzisin86@gmail.com>
2020-06-18 17:26:50 +0300

    Add project files.

---
+++ WindowsFormsAppShop/Form1.Designer.cs
@@ -0,0 +1,347 @@
+﻿namespace WindowsFormsAppShop
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBoxCity = new System.Windows.Forms.TextBox();
+            this.textBoxStreet = new System.Windows.Forms.TextBox();
+            this.textBoxZipCode = new System.Windows.Forms.TextBox();
+            this.textBoxPhoneNumber = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBoxName = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textBoxVentorNumberInventory = new System.Windows.Forms.TextBox();
+            this.textBoxCurrenQuantity = new System.Windows.Forms.TextBox();
+            this.textBoxDescription = new System.Windows.Forms.TextBox();
+            this.textBoxItemId = new System.Windows.Forms.TextBox();
+            this.textBoxItem = new System.Windows.Forms.TextBox();
+            this.textBoxVentorNumber = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(420, 287);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(891, 171);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick_1);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(1052, 106);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Add";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Add_Click1);
+            // 
+            // textBoxCity
+            // 
+            this.textBoxCity.Location = new System.Drawing.Point(754, 102);
+            this.textBoxCity.Name = "textBoxCity";
+            this.textBoxCity.Size = new System.Drawing.Size(100, 22);
+            this.textBoxCity.TabIndex = 3;
+            // 
+            // textBoxStreet
+            // 
+            this.textBoxStreet.Location = new System.Drawing.Point(754, 149);
+            this.textBoxStreet.Name = "textBoxStreet";
+            this.textBoxStreet.Size = new System.Drawing.Size(100, 22);
+            this.textBoxStreet.TabIndex = 4;
+            // 
+            // textBoxZipCode
+            // 
+            this.textBoxZipCode.Location = new System.Drawing.Point(754, 193);
+            this.textBoxZipCode.Name = "textBoxZipCode";
+            this.textBoxZipCode.Size = new System.Drawing.Size(100, 22);
+            this.textBoxZipCode.TabIndex = 5;
+            // 
+            // textBoxPhoneNumber
+            // 
+            this.textBoxPhoneNumber.Location = new System.Drawing.Point(754, 231);
+            this.textBoxPhoneNumber.Name = "textBoxPhoneNumber";
+            this.textBoxPhoneNumber.Size = new System.Drawing.Size(100, 22);
+            this.textBoxPhoneNumber.TabIndex = 6;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(634, 105);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 17);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "City:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(634, 149);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(50, 17);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Street:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(634, 193);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(65, 17);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "ZipCode:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(634, 231);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(103, 17);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "PhoneNumber:";
+            // 
+            // textBoxName
+            // 
+            this.textBoxName.Location = new System.Drawing.Point(754, 63);
+            this.textBoxName.Name = "textBoxName";
+            this.textBoxName.Size = new System.Drawing.Size(100, 22);
+            this.textBoxName.TabIndex = 12;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(634, 63);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(49, 17);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "Name:";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(975, 532);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 11;
+            this.button2.Text = "Add";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Add_Click2);
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(425, 701);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.RowTemplate.Height = 24;
+            this.dataGridView2.Size = new System.Drawing.Size(886, 171);
+            this.dataGridView2.TabIndex = 10;
+            this.dataGridView2.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView2_CellContentClick);
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(612, 658);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(108, 17);
+            this.label11.TabIndex = 9;
+            this.label11.Text = "Ventor Number:";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(612, 621);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(116, 17);
+            this.label10.TabIndex = 8;
+            this.label10.Text = "Current Quantity:";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(612, 582);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(83, 17);
+            this.label9.TabIndex = 7;
+            this.label9.Text = "Description:";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(612, 538);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(49, 17);
+            this.label8.TabIndex = 6;
+            this.label8.Text = "ItemId:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(612, 495);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(38, 17);
+            this.label7.TabIndex = 5;
+            this.label7.Text = "Item:";
+            // 
+            // textBoxVentorNumberInventory
+            // 
+            this.textBoxVentorNumberInventory.Location = new System.Drawing.Point(754, 658);
+            this.textBoxVentorNumberInventory.Name = "textBoxVentorNumberInventory";
+            this.textBoxVentorNumberInventory.Size = new System.Drawing.Size(100, 22);
+            this.textBoxVentorNumberInventory.TabIndex = 4;
+            // 
+            // textBoxCurrenQuantity
+            // 
+            this.textBoxCurrenQuantity.Location = new System.Drawing.Point(754, 618);
+            this.textBoxCurrenQuantity.Name = "textBoxCurrenQuantity";
+            this.textBoxCurrenQuantity.Size = new System.Drawing.Size(100, 22);
+            this.textBoxCurrenQuantity.TabIndex = 3;
+            // 
+            // textBoxDescription
+            // 
+            this.textBoxDescription.Location = new System.Drawing.Point(754, 577);
+            this.textBoxDescription.Name = "textBoxDescription";
+            this.textBoxDescription.Size = new System.Drawing.Size(100, 22);
+            this.textBoxDescription.TabIndex = 2;
+            // 
+            // textBoxItemId
+            // 
+            this.textBoxItemId.Location = new System.Drawing.Point(754, 533);
+            this.textBoxItemId.Name = "textBoxItemId";
+            this.textBoxItemId.Size = new System.Drawing.Size(100, 22);
+            this.textBoxItemId.TabIndex = 1;
+            // 
+            // textBoxItem
+            // 
+            this.textBoxItem.Location = new System.Drawing.Point(754, 490);
+            this.textBoxItem.Name = "textBoxItem";
+            this.textBoxItem.Size = new System.Drawing.Size(100, 22);
+            this.textBoxItem.TabIndex = 0;
+            // 
+            // textBoxVentorNumber
+            // 
+            this.textBoxVentorNumber.Location = new System.Drawing.Point(754, 31);
+            this.textBoxVentorNumber.Name = "textBoxVentorNumber";
+            this.textBoxVentorNumber.Size = new System.Drawing.Size(100, 22);
+            this.textBoxVentorNumber.TabIndex = 14;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(634, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(108, 17);
+            this.label1.TabIndex = 15;
+            this.label1.Text = "Ventor Number:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1754, 931);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxVentorNumber);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBoxName);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.textBoxDescription);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.textBoxCity);
+            this.Controls.Add(this.textBoxItem);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.textBoxStreet);
+            this.Controls.Add(this.textBoxItemId);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.textBoxZipCode);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBoxCurrenQuantity);
+            this.Controls.Add(this.textBoxPhoneNumber);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.textBoxVentorNumberInventory);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBoxCity;
+        private System.Windows.Forms.TextBox textBoxStreet;
+        private System.Windows.Forms.TextBox textBoxZipCode;
+        private System.Windows.Forms.TextBox textBoxPhoneNumber;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBoxName;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox textBoxVentorNumberInventory;
+        private System.Windows.Forms.TextBox textBoxCurrenQuantity;
+        private System.Windows.Forms.TextBox textBoxDescription;
+        private System.Windows.Forms.TextBox textBoxItemId;
+        private System.Windows.Forms.TextBox textBoxItem;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.TextBox textBoxVentorNumber;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ WindowsFormsAppShop/Form1.cs
@@ -0,0 +1,275 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsAppShop
+{
+    public partial class Form1 : Form
+    {
+        private string connStr = ConfigurationManager.ConnectionStrings["myConnectionString"].ConnectionString;
+        private readonly DataGridViewButtonColumn deleteBtn1 = new DataGridViewButtonColumn();
+        private readonly DataGridViewButtonColumn deleteBtn2 = new DataGridViewButtonColumn();
+  
+        public Form1()
+        {
+            InitializeComponent();
+            ConstructDataGridView1();
+            ConstructDataGridView2();
+        }
+        private void ConstructDataGridView1()
+        {
+            //ADD COLUMNS
+            dataGridView1.ColumnCount = 6;
+            dataGridView1.Columns[0].Name = "VendorNumber";
+            dataGridView1.Columns[1].Name = "Name";
+            dataGridView1.Columns[2].Name = "City";
+            dataGridView1.Columns[3].Name = "Street";
+            dataGridView1.Columns[4].Name = "ZipCode";
+            dataGridView1.Columns[5].Name = "PhoneNumber";
+
+            dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            AddDeleteButtonColumn1();
+            dataGridView1.Columns.Add(deleteBtn1);
+            AddRowVentors();
+        }
+        private void ConstructDataGridView2()
+        {
+            //ADD COLUMNS
+            dataGridView2.ColumnCount = 5;
+            dataGridView2.Columns[0].Name = "ItemId";
+            dataGridView2.Columns[1].Name = "Item";
+            dataGridView2.Columns[2].Name = "CurrentQuantity";
+            dataGridView2.Columns[3].Name = "Description";
+            dataGridView2.Columns[4].Name = "VentorNumber";
+
+            dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            AddDeleteButtonColumn2();
+            dataGridView2.Columns.Add(deleteBtn2);
+            AddRowInventory();
+        }
+        private void AddRowVentors()
+        {
+            foreach (Ventor vd in GetAllVentors())
+            {
+                dataGridView1.Rows.Add(vd.ventorNumber, vd.Name, vd.City, vd.Street, vd.ZipCode, vd.PhoneNumber);
+            }
+        }
+        private void AddRowInventory()
+        {
+            foreach (Inventory i in GetAllInventory())
+            {
+                dataGridView2.Rows.Add(i.ItemId, i.Item, i.CurrentQuantity, i.Description, i.VentorNumber);
+            }
+        }
+        private void AddDeleteButtonColumn1()
+        {
+            deleteBtn1.HeaderText = @"";
+            deleteBtn1.Name = "DeleteButton";
+            deleteBtn1.Text = "Delete";
+            deleteBtn1.UseColumnTextForButtonValue = true;
+        }
+        private void AddDeleteButtonColumn2()
+        {
+            deleteBtn2.HeaderText = @"";
+            deleteBtn2.Name = "DeleteButton";
+            deleteBtn2.Text = "Delete";
+            deleteBtn2.UseColumnTextForButtonValue = true;
+        }
+        public List<Ventor> GetAllVentors()
+        {
+            List<Ventor> ventorDetails = new List<Ventor>();
+            using (SqlConnection conn = new SqlConnection(connStr))
+            {
+                string query = "SELECT * FROM Ventor";
+                SqlCommand cmd = new SqlCommand(query, conn);
+                conn.Open();
+                using (SqlDataReader reader = cmd.ExecuteReader())
+                {
+                    while (reader.Read() == true)
+                    {
+                        Ventor vd = new Ventor
+                        {
+                            ventorNumber = ((int)reader["VentorNumber"]).ToString(),
+                            Name = reader["Name"].ToString().Trim(),
+                            City = reader["City"].ToString().Trim(),
+                            Street = reader["Street"].ToString().Trim(),
+                            ZipCode = reader["ZipCode"].ToString().Trim(),
+                            PhoneNumber = reader["PhoneNumber"].ToString().Trim(),
+
+                        };
+                        ventorDetails.Add(vd);
+                    }
+                }
+            }
+            return ventorDetails;
+
+        }
+        public List<Inventory> GetAllInventory()
+        {
+            List<Inventory> inventory = new List<Inventory>();
+            using (SqlConnection conn = new SqlConnection(connStr))
+            {
+                string query = "SELECT * FROM Inventory";
+                SqlCommand cmd = new SqlCommand(query, conn);
+                conn.Open();
+                using (SqlDataReader reader = cmd.ExecuteReader())
+                {
+                    while (reader.Read() == true)
+                    {
+                        Inventory i = new Inventory
+                        {
+                            VentorNumber = ((int)reader["VentorNumber"]).ToString(),
+                            CurrentQuantity = reader["CurrentQuantity"].ToString().Trim(),
+                            Item = reader["Item"].ToString().Trim(),
+                            ItemId = (int)reader["ItemId"],
+                            Description = reader["Description"].ToString().Trim()
+
+                        };
+                        inventory.Add(i);
+                    }
+                }
+            }
+            return inventory;
+
+        }
+
+        private void DeleteVentor()
+        {
+            string ventorNumber = (string)dataGridView1.CurrentRow.Cells[0].Value;
+            using (SqlConnection conn = new SqlConnection(connStr))
+            {
+                string query = $"DELETE FROM Ventor WHERE VentorNumber={ventorNumber}";
+                conn.Open();
+                SqlCommand cmd = new SqlCommand(query, conn);
+                cmd.ExecuteNonQuery();
+            }
+            dataGridView1.Rows.Clear();
+            ConstructDataGridView1();
+        }
+        private void DeleteItem()
+        {
+            string ItemId = (string)dataGridView2.CurrentRow.Cells[0].Value;
+            using (SqlConnection conn = new SqlConnection(connStr))
+            {
+                string query = $"DELETE FROM Inventory WHERE ItemId={ItemId}";
+                conn.Open();
+                SqlCommand cmd = new SqlCommand(query, conn);
+                cmd.ExecuteNonQuery();
+            }
+            dataGridView1.Rows.Clear();
+            ConstructDataGridView1();
+        }
+
+        private void dataGridView1_CellContentClick_1(object sender, DataGridViewCellEventArgs e)
+        {
+            if (dataGridView1.Columns[e.ColumnIndex].Name == "DeleteButton")
+            {
+                if (MessageBox.Show("Are you sure want to delete this record?", "Message", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+                {
+                    DeleteVentor();
+                }
+
+            }
+        }
+        private void dataGridView2_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (dataGridView2.Columns[e.ColumnIndex].Name == "DeleteButton")
+            {
+                if (MessageBox.Show("Are you sure want to delete this record?", "Message", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+                {
+                    DeleteItem();
+                }
+
+            }
+        }
+      
+        private void Add_Click1(object sender, EventArgs e)
+        {
+        //string Message;
+        Ventor v = new Ventor();
+        {
+            v.ventorNumber = textBoxVentorNumber.Text;
+            v.Name = textBoxName.Text;
+            v.City = textBoxCity.Text;
+            v.Street = textBoxStreet.Text;
+            v.ZipCode = textBoxZipCode.Text;
+            v.PhoneNumber = textBoxPhoneNumber.Text;
+        };
+            using (SqlConnection conn = new SqlConnection(connStr))
+            {
+                StringBuilder sb = new StringBuilder();
+                sb.Append("INSERT INTO Ventor(VentorNumber, Name, City, Street, ZipCode, PhoneNumber) values(@VentorNumber, @Name, @City, @Street, @ZipCode, @PhoneNumber)");
+                string query = sb.ToString();
+                conn.Open();
+                SqlCommand cmd = new SqlCommand(query, conn);
+                cmd.Parameters.AddWithValue("@VentorNumber", v.ventorNumber);
+                cmd.Parameters.AddWithValue("@Name", v.Name);
+                cmd.Parameters.AddWithValue("@City", v.City);
+                cmd.Parameters.AddWithValue("@Street", v.Street);
+                cmd.Parameters.AddWithValue("@ZipCode", v.ZipCode);
+                cmd.Parameters.AddWithValue("@PhoneNumber", v.PhoneNumber);
+                cmd.ExecuteNonQuery();
+                dataGridView1.Rows.Clear();
+                ConstructDataGridView1();
+            }
+        }
+
+        private void Add_Click2(object sender, EventArgs e)
+        {
+            //string Message;
+            Inventory i = new Inventory();
+            {
+                i.VentorNumber = textBoxVentorNumberInventory.Text;
+                i.Item = textBoxItem.Text;
+                i.ItemId = Convert.ToInt32(textBoxItemId.Text);
+                i.Description = textBoxDescription.Text;
+                i.CurrentQuantity = textBoxCurrenQuantity.Text;
+            
+            };
+            using (SqlConnection conn = new SqlConnection(connStr))
+            {
+                StringBuilder sb = new StringBuilder();
+                sb.Append("INSERT INTO Inventory(VentorNumber, Item, ItemId, Description, CurrentQuantity) values(@VentorNumber, @Item, @ItemId, @Description, @CurrentQuantity)");
+                string query = sb.ToString();
+                conn.Open();
+                SqlCommand cmd = new SqlCommand(query, conn);
+                cmd.Parameters.AddWithValue("@VentorNumber", i.VentorNumber);
+                cmd.Parameters.AddWithValue("@Item", i.Item);
+                cmd.Parameters.AddWithValue("@ItemId", i.ItemId);
+                cmd.Parameters.AddWithValue("@Description", i.Description);
+                cmd.Parameters.AddWithValue("@CurrentQuantity", i.CurrentQuantity);
+             
+                cmd.ExecuteNonQuery();
+                dataGridView2.Rows.Clear();
+                ConstructDataGridView2();
+            }
+        }
+    }
+    public class Ventor
+    {
+        public string ventorNumber { get; set; }
+        public string Name { get; set; }
+        public string City { get; set; }
+        public string Street { get; set; }
+        public string ZipCode { get; set; }
+        public string PhoneNumber { get; set; }
+    }
+    public class Inventory
+    {
+        public int ItemId { get; set; }
+        public string Item { get; set; }
+        public string Description { get; set; }
+        public string CurrentQuantity { get; set; }
+        public string VentorNumber { get; set; }
+    }
+}
